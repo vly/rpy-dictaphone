83845f2ccd4227aa918f8fcf5bd0c76e52df829b
neilang <neilang@gmail.com>
2013-10-06 13:38:34 +1100

    Cleaned up code

--- src/pir-monitor/main.c
+++ src/pir-monitor/main.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 #include <time.h>
 
-#undef NA_PIN RPI_GPIO_P1_07
+#define NA_PIN RPI_GPIO_P1_07
 #define NA_POLL_DELAY 33
 #define NA_PIN_STILL_VALUE 1
 
@@ -14,23 +14,19 @@ int main(int argc, char **argv)
         return EXIT_FAILURE;
     }
 
-    // Setup pin to use
     bcm2835_gpio_fsel(NA_PIN, BCM2835_GPIO_FSEL_INPT);
 
-    // Use pull up
     bcm2835_gpio_set_pud(NA_PIN, BCM2835_GPIO_PUD_UP);
 
     uint8_t value = NA_PIN_STILL_VALUE;
 
     while (1) {
-        // Read pin value
         value = bcm2835_gpio_lev(NA_PIN);
 
         if(value != NA_PIN_STILL_VALUE){
             printf("%d\n", (int)time(NULL));
         }
 
-        // Pause before polling again
         delay(NA_POLL_DELAY);
     }
 
